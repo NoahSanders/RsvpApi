6886543c48ab2b774559912c5de6648b8e2ca3be
Noah Sanders <noahcsan@gmail.com>
2017-08-31 21:37:15 -0500

    work on API - update guest function and error handling

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -2,6 +2,7 @@
 const mysql = require('mysql');
 const sanitizer = require('sanitizer');
 const login = require('./login.json');
+const util = require('util');
 
 exports.get_all_guests = function(req, res) {
     var connection = getConnection();
@@ -31,18 +32,55 @@ exports.create_guest = function(request, response) {
 }
 
 exports.get_guest = function(req, res) {
-    var connection = getConnection();
+    try {
+        var connection = getConnection();
 
-    connection.end((err) => {
-        console.log('Error ending db connection ' + err.code + ' ' + message);
-    });
+        connection.end((err) => {
+            console.log('Error ending db connection ' + err.code + ' ' + message);
+        });
+    }
+    catch (exception) {
+        console.log("Error updating guest: " + util.inspect(exception));
+        if (!response.finished) response.status(500).json(exception);
+    }
 }
-exports.update_guest = function(req, res) {
-    var connection = getConnection();
 
-    connection.end((err) => {
-        console.log('Error ending db connection ' + err.code + ' ' + message);
-    });
+exports.update_guest = function(req, response) {
+    try {
+        var guestId = req.params.guestId;
+        var allowedFields = ['name', 'address_1', 'address_2', 'city', 'state', 'zip_code', 'suggested_guest_count', 'guest_count', 'status', 'last_name'];
+        
+        if (req.body.id && (req.body.id !== guestId)) {
+            response.status(400).json({"message": "Updating the id of a guest is not allowed!"});
+            throw {"name": "BadRequestMessage", "source": "update_guest", "message": req.params + req.body};
+        }
+
+        for (var field in req.body) {
+            if (!allowedFields.includes(field)) {
+                response.status(400).json({"message": "Incorrect field name included in request: " + field});
+                throw {"name": "BadRequestMessage", "source": "update_guest", "message": req.params + req.body};
+            }
+        }
+
+        var connection = getConnection();    
+
+        if (req.params.guestId) {
+            connection.query('UPDATE guest SET ? WHERE ID = ?', [req.body, parseInt(req.params.guestId)], (db_error, db_response) => {
+                if (db_error) throw {"name": "DatabaseError", "source": "update_guest", "message": 'Error updating ID ' + req.params.guestId + ' - '+ req.body + ' - ' + db_error};
+
+                console.log('Updated ID ' + guestId);
+                response.sendStatus(200);
+            });
+        }
+
+        connection.end((err) => {
+            if (err) throw {"name": "DatabaseError", "source": "update_guest", "message":'Error ending db connection ' + err};
+        });
+    }
+    catch (exception) {
+        console.log("Error updating guest: " + util.inspect(exception));
+        if (!response.finished) response.status(500).json(exception);
+    }
 }
 
 function getConnection() {
@@ -52,7 +90,6 @@ function getConnection() {
             console.log('Error connecting to db ' + err.code + ' ' + err.message);
             throw err;
         }
-        console.log('Connected to db');
     });
     return connection;
 }
